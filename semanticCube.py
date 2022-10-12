8ae348c9e649446cbf4884760eeb62fa66a47dec
Citlalli27 <70440578+Citlalli27@users.noreply.github.com>
2022-10-11 19:12:27 -0500

    Tabla de variables, directorio de funciones, mover el cubo semantico

---
+++ semanticCube.py
@@ -0,0 +1,227 @@
+class SemanticCube:
+    def __init__(self):
+        self.cube = {
+            'int':{
+                'int':{
+                    '+': 'int',
+                    '-': 'int',
+                    '/': 'float',
+                    '*': 'int',
+                    '<': 'bool',
+                    '>': 'bool',
+                    'and': 'err',
+                    'or': 'err',
+                    'not': 'bool',
+                    'equal': 'bool',
+                    '=': True
+                    },
+                'float':{
+                    '+': 'float',
+                    '-': 'float',
+                    '/': 'float',
+                    '*': 'float',
+                    '<': 'bool',
+                    '>': 'bool',
+                    'and': 'err',
+                    'or': 'err',
+                    'not': 'bool',
+                    'equal': 'bool',
+                    '=': False
+                },
+                'bool':{
+                    '+': 'err',
+                    '-': 'err',
+                    '/': 'err',
+                    '*': 'err',
+                    '<': 'err',
+                    '>': 'err',
+                    'and': 'err',
+                    'or': 'err',
+                    'not': 'err',
+                    'equal': 'err',
+                    '=': False
+                },
+                'char':{
+                    '+': 'err',
+                    '-': 'err',
+                    '/': 'err',
+                    '*': 'err',
+                    '<': 'err',
+                    '>': 'err',
+                    'and': 'err',
+                    'or': 'err',
+                    'not': 'err',
+                    'equal': 'err',
+                    '=': False
+                }
+             },
+
+            'float':{
+                'int':{
+                    '+': 'float',
+                    '-': 'float',
+                    '/': 'float',
+                    '*': 'float',
+                    '<': 'bool',
+                    '>': 'bool',
+                    'and': 'err',
+                    'or': 'err',
+                    'not': 'bool',
+                    'equal': 'bool',
+                    '=': True
+                },
+                'float':{
+                    '+': 'float',
+                    '-': 'float',
+                    '/': 'float',
+                    '*': 'float',
+                    '<': 'bool',
+                    '>': 'bool',
+                    'and': 'err',
+                    'or': 'err',
+                    'not': 'bool',
+                    'equal': 'bool',
+                    '=': True
+                },
+                'bool':{
+                    '+': 'err',
+                    '-': 'err',
+                    '/': 'err',
+                    '*': 'err',
+                    '<': 'err',
+                    '>': 'err',
+                    'and': 'err',
+                    'or': 'err',
+                    'not': 'err',
+                    'equal': 'err',
+                    '=': False
+                },
+                'char':{
+                    '+': 'err',
+                    '-': 'err',
+                    '/': 'err',
+                    '*': 'err',
+                    '<': 'err',
+                    '>': 'err',
+                    'and': 'err',
+                    'or': 'err',
+                    'not': 'err',
+                    'equal': 'err',
+                    '=': False
+                }
+            },
+
+            'bool':{
+                'int':{
+                    '+': 'err',
+                    '-': 'err',
+                    '/': 'err',
+                    '*': 'err',
+                    '<': 'err',
+                    '>': 'err',
+                    'and': 'err',
+                    'or': 'err',
+                    'not': 'err',
+                    'equal': 'err',
+                    '=': False
+                },
+                'float':{
+                    '+': 'err',
+                    '-': 'err',
+                    '/': 'err',
+                    '*': 'err',
+                    '<': 'err',
+                    '>': 'err',
+                    'and': 'err',
+                    'or': 'err',
+                    'not': 'err',
+                    'equal': 'err',
+                    '=': False
+                },
+                'bool':{
+                    '+': 'err',
+                    '-': 'err',
+                    '/': 'err',
+                    '*': 'err',
+                    '<': 'err',
+                    '>': 'err',
+                    'and': 'bool',
+                    'or': 'bool',
+                    'not': 'bool',
+                    'equal': 'bool',
+                    '=': True
+                },
+                'char':{
+                    '+': 'err',
+                    '-': 'err',
+                    '/': 'err',
+                    '*': 'err',
+                    '<': 'err',
+                    '>': 'err',
+                    'and': 'err',
+                    'or': 'err',
+                    'not': 'err',
+                    'equal': 'err',
+                    '=': False
+                }
+            },
+            'char':{
+                'int':{
+                    '+': 'err',
+                    '-': 'err',
+                    '/': 'err',
+                    '*': 'err',
+                    '<': 'err',
+                    '>': 'err',
+                    'and': 'err',
+                    'or': 'err',
+                    'not': 'err',
+                    'equal': 'err',
+                    '=': False
+            },
+                'float':{
+                    '+': 'err',
+                    '-': 'err',
+                    '/': 'err',
+                    '*': 'err',
+                    '<': 'err',
+                    '>': 'err',
+                    'and': 'err',
+                    'or': 'err',
+                    'not': 'err',
+                    'equal': 'err',
+                    '=': False
+                },
+                'bool':{
+                    '+': 'err',
+                    '-': 'err',
+                    '/': 'err',
+                    '*': 'err',
+                    '<': 'err',
+                    '>': 'err',
+                    'and': 'err',
+                    'or': 'err',
+                    'not': 'err',
+                    'equal': 'err',
+                    '=': False
+                },
+                'char':{
+                    '+': 'err',
+                    '-': 'err',
+                    '/': 'err',
+                    '*': 'err',
+                    '<': 'err',
+                    '>': 'err',
+                    'and': 'err',
+                    'or': 'err',
+                    'not': 'bool',
+                    'equal': 'bool',
+                    '=': True
+                }
+            }
+    }
+
+def semanticCube (self, oper1, oper2, operador):
+    if oper1 in self.cube and oper2 in self.cube[oper1] and operador in self.cube[oper1][oper2]:
+        return self.cube[oper1][oper2][operador], None
+    else: return None, "Type mismatch"
--- varTable.py
+++ varTable.py
@@ -1,5 +1,5 @@
-class varTable:
-    def __init__(self):
+#class varTable:
+""" def __init__(self):
         self.tabla = {
             0 : {
                 "bloque_codigo" : "program", "nombre" : "proyectocompilador", "tipo" : "program", "vars" : {}
@@ -12,5 +12,28 @@ class varTable:
         self.tabla[num]["nombre"] = nombre
         self.tabla[num]["tipo"] = tipo
         self.tabla[num]["vars"] = {}
+"""
+
+
+#Function Directory 
+from variable import Var
+
+class varTable:
+    def __init_(self):
+        self.table = {}
+    
+    def add(self, name, type, scope):
+        currentVar = Var(type, scope)
+        if name in self.table:
+            return "The variable already exists"
+        else:
+            self.table[name] = currentVar
+            return None
+    
+    def search(self, name):
+        if name in self.table:
+            return self.table[name], None
+        else: return None, "Variable undeclared"
+    
 
     
